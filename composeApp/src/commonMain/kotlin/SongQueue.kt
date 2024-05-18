afcda9b7e7e60e00f342ac8f6892af6f890858aa
Jake Adamson <Jake-The-Human@users.noreply.github.com>
2024-05-18 12:03:05 -0400

    added fake audio controls

--- composeApp/src/commonMain/kotlin/SongQueue.kt
+++ composeApp/src/commonMain/kotlin/SongQueue.kt
@@ -1,12 +1,15 @@
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.ktor.client.HttpClient
@@ -42,8 +45,11 @@ import kotlinx.coroutines.runBlocking
             Card(modifier = Modifier.padding(4.dp).fillMaxWidth().weight(1F)) {
                 Column {
                     Text(text = "now playing")
-                    Text(text = "Title place holder")
-                    Text(text = "artist place holder")
+                    Row (modifier = Modifier.padding(4.dp).align(Alignment.CenterHorizontally)){
+                        Button(modifier = Modifier.padding(4.dp, 0.dp), onClick = {}) { Text("⏮️") }
+                        Button(modifier = Modifier.padding(4.dp, 0.dp), onClick = {}) {Text("⏯️")}
+                        Button(modifier = Modifier.padding(4.dp, 0.dp), onClick = {}) {Text("⏭️")}
+                    }
                 }
             }
         }
